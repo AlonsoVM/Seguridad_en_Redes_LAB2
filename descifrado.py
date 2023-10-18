e229c886091c107a33eb6007f484893c27b4d3cb
AlonsoVM <Alonso.Villamayor@alu.uclm.es>
2023-10-18 19:44:14 +0200

    archivos varios

--- descifrado.py
+++ descifrado.py
@@ -23,26 +23,26 @@ def contiene_palabras_con_numeros(linea):
             return True
     return False
 
+"""
 with open(archivo_pass, "rb") as archivo:
     # Leer cada línea del archivo
     for linea in archivo:
-        """Decodificar la linea"""
         l_aux = linea.decode(errors="ignore")
         l_aux = l_aux.lower().strip()
         if str(l_aux).isascii() and not contiene_palabras_con_numeros(l_aux) and re.match(r'^[a-z]+$', l_aux):
             lineas_procesadas.append(l_aux)
+"""
 
 def divideLista(lista:list, tamanno:int):
     tamannoSublista = len(lista) // (tamanno-1)
     sublistas = [lista[i:i + tamannoSublista] for i in range(0, len(lista), tamannoSublista)]
     return sublistas
     
-def ProbarContraseña(id:int, combinations:list, f):
-    i = 0
-    gpg = gnupg.GPG()
+def ProbarContraseña(id:int, combinations:list, data):
+
     for password in combinations:
-        i=i+1
-        resultado = gpg.decrypt_file("archivo_cifrado.gpg", passphrase=password)
+
+        resultado = gpg.decrypt(data, passphrase=password)
         
         if resultado.ok:
             print(f"Contraseña válida, iteración{i} desde el hilo:{id}")
@@ -53,31 +53,23 @@ def ProbarContraseña(id:int, combinations:list, f):
             lock.release()
             break
         if contrasennaEncontrada: break
-        print(f"contraseña invalida iteración: {i} desde el hilo{id} pass probada:{password}")
         
 
 
 hilos = []
-#combinations = [''.join(combination) for combination in itertools.product(characters, repeat=3)]
-listas_hilos = divideLista(lineas_procesadas, 10)
-print(len(listas_hilos))
-listas_hilos[1].insert(50, "qwerty")
-
-descriptores = []
-gpgs = []
-
-for i in range(0,9):
-    print(i)
-    archivo = open("archivo_cifrado.gpg", "rb")
-
-    descriptores.append(archivo)
-
-for i in range(0, 9):
-    hilo = threading.Thread(target=ProbarContraseña, args=(i, listas_hilos[i], descriptores[i],))
-    hilos.append(hilo)
-    hilo.start()
+f = open("archivo_cifrado.gpg", "rb")
+data = f.read()
+gpg = gnupg.GPG()
+combinations = [''.join(combination) for combination in itertools.product(characters, repeat=2)]
+listas_hilos = divideLista(combinations, 8)
+listas_hilos[2].append("qwerty")
+for i in range(0, len(listas_hilos)):
+    hilos.append(threading.Thread(target=ProbarContraseña, args=(i, listas_hilos[i], data)))
 t1 = time.time()
-for hilo in hilos:
-    hilo.join();
+for i in range(0, len(hilos)):
+    hilos[i].start();
+
+for i in range(0, len(hilos)):
+    hilos[i].join();
 t2 = time.time()
 print(f'Contraseña encontrada :{contrasenna} Tiempo utilizado:{t2-t1}')
